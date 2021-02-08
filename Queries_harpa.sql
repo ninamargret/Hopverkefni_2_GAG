71d4ef4a870227ec5ad5c39f47c5ec351f863b7f
harpasteingrims <harpastein@gmail.com>
2021-02-08 15:11:33 +0000

    Added new queries.sql

---
+++ Queries_harpa.sql
@@ -0,0 +1,21 @@
+select 1 as Query;
+
+-- select ...
+
+select 3 as Query;
+
+-- select ...
+
+select 5 as Query;
+
+-- select ...
+
+select 7 as Query;
+
+-- select ...
+
+select 9 as Query;
+
+-- select ...
+
+
--- Queries_nina.sql
+++ Queries_nina.sql
@@ -1,12 +1,5 @@
-select 1 as Query;
-
--- select ...
 
 select 2 as Query;
-
--- select ...
-
-select 3 as Query;
 
 -- select ...
 
@@ -14,23 +7,11 @@ select 4 as Query;
 
 -- select ...
 
-select 5 as Query;
-
--- select ...
-
 select 6 as Query;
 
 -- select ...
 
-select 7 as Query;
-
--- select ...
-
 select 8 as Query;
-
--- select ...
-
-select 9 as Query;
 
 -- select ...
 
