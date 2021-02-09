f82e2e28b8ae5a94560a86c2ad8d8d71335ab4d1
Nina Margrét <nina19@ru.is>
2021-02-09 22:50:47 +0000

    Do 2,4 and 6

--- Queries_nina.sql
+++ Queries_nina.sql
@@ -1,21 +1,38 @@
-
 select 2 as Query;
-
--- select ...
+-- The name of all locations with at least one case, sorted in ascending order.
+SELECT location, caseCount
+FROM  Locations
+WHERE caseCount >= 1 --Er at least < eða >=??
+ORDER BY location ASC, caseCount;
 
 select 4 as Query;
+-- The secret identity name of agents who have investigated people more than ten times
+SELECT A.codename
+FROM Agents A 
+JOIN InvolvedIn I ON A.AgentID = I.AgentID
+GROUP BY A.codename
+HAVING COUNT(I.PersonID) > 10;
 
--- select ...
 
 select 6 as Query;
+-- The ID, name and gender of all people who are involved in a case in Selfoss.
 
--- select ...
+SELECT P.PersonID, P.name, G.gender
+FROM Genders G 
+JOIN People P 
+    ON G.GenderID = P.GenderID
+JOIN InvolvedIn I 
+    ON I.PersonID = P.PersonID
+JOIN Cases C
+    ON I.CaseID = C.CaseID
+JOIN Locations L ON L.LocationID = C.LocationID
+WHERE L.location = 'Selfoss';
 
 select 8 as Query;
 
--- select ...
+-- The codename, gender and password of all agents that	have a weak password. A	password is	weak if the agent's codename can be	found in their password
 
 select 10 as Query;
 
--- select ...
+-- For people that have	been	investigated	by exactly 3	different	agents, select their ID, name and gender as well as how	 many	 years 	it has	been	since	they were last investigated. The last column should	 be named “yearsSinceLastInvestigation” Please note that some people	 involved in a crime	 are not investigated, and that The Bureau is operating	in the current year: 2045.
 
