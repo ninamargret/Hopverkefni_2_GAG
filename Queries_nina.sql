9fe58e61c25064b3724d1476ca09c5a373f8d2ec
Nina Margrét <nina19@ru.is>
2021-02-10 00:03:43 +0000

    Do 8 and try 10

--- Queries_nina.sql
+++ Queries_nina.sql
@@ -29,10 +29,30 @@ JOIN Locations L ON L.LocationID = C.LocationID
 WHERE L.location = 'Selfoss';
 
 select 8 as Query;
-
 -- The codename, gender and password of all agents that	have a weak password. A	password is	weak if the agent's codename can be	found in their password
+SELECT A.codename, G.gender, P.Password
+FROM Passwords P
+JOIN Agents A 
+    ON P.AgentID = A.AgentID
+JOIN Genders G
+    ON G.GenderID = A.GenderID
+WHERE P.Password = '%'+ A.codename+ '%'; -- HJÁLP
+
 
 select 10 as Query;
+-- For people that have	been investigated by exactly 3 different agents, 
+-- select their ID, name and gender as well as how  many years it has been since they were last investigated. 
+-- The last column should be named “yearsSinceLastInvestigation” 
+-- Please note that some people	involved in a crime are not investigated, and that The Bureau is operating in the current year: 2045.
 
--- For people that have	been	investigated	by exactly 3	different	agents, select their ID, name and gender as well as how	 many	 years 	it has	been	since	they were last investigated. The last column should	 be named “yearsSinceLastInvestigation” Please note that some people	 involved in a crime	 are not investigated, and that The Bureau is operating	in the current year: 2045.
+SELECT P.PersonID, P.name, G.gender, “yearsSinceLastInvestigation”
+FROM Genders G 
+JOIN People P 
+    ON G.GenderID = P.GenderID
+JOIN InvolvedIn I 
+    ON I.PersonID = P.PersonID
+JOIN Cases C
+    ON I.CaseID = C.CaseID 
 
+
+--HJÁLP hvernig reikna ég
